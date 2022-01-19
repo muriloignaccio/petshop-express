200917568764b794c2fa2fb0df9ceb3b80dab9bc
muriloignaccio <murilohsignacio@gmail.com>
2022-01-19 20:48:58 -0300

    Converter HTML para EJS

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 const petsRouter = require('./routes/pets');
+const servicosRouter = require('./routes/servicos');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/pets', petsRouter);
+app.use('/servicos', servicosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
