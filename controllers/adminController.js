8e40f996a5a1c2d44f281a5dac943a18ba4a9611
muriloignaccio <murilohsignacio@gmail.com>
2022-01-31 19:10:30 -0300

    Criar página de cadastro

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,33 @@
+const servicosModel = require('../models/servicos.json');
+const uuidv4 = require('uuidv4');
+const fs = require('fs');
+
 const adminController = {
   index: function(req, res) {
     res.render('admin');
+  }, 
+  buscarServicos: function(req, res) {
+    res.render('adminServicos', { servicos: servicosModel })
+  },
+  cadastrarServicos: function(req, res) {
+    res.render('servicosCadastro')
+  },
+  store: function(req, res) {
+    const { nome, preco, descricao } = req.body;
+
+    const servico = {
+      id: uuidv4.uuid(),
+      nome,
+      preco,
+      descricao,
+      fotoURL: ''
+    }
+
+    servicosModel.push(servico);
+
+    fs.writeFileSync(__dirname + '/../models/servicos.json', JSON.stringify(servicosModel));
+
+    res.redirect('/admin/servicos');
   }
 };
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,16 @@ const express = require('express');
 const adminController = require('../controllers/adminController');
 const router = express.Router();
 
+const path = require('path');
+
+console.log(path.resolve('uploads'))
+
 router.get('/', adminController.index);
+
+router.get('/servicos', adminController.buscarServicos);
+
+router.get('/servicos/cadastrar', adminController.cadastrarServicos);
+
+router.post('/servicos/cadastrar', adminController.store);
 
 module.exports = router;
