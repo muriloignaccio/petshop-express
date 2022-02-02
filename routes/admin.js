4e58b91092d33ffc5c6fc81cf24a20050334b036
muriloignaccio <murilohsignacio@gmail.com>
2022-02-02 19:53:40 -0300

    Adicionar input na página de editar

--- routes/admin.js
+++ routes/admin.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 
-// console.log(path.resolve('/uplo'))
-
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, path.resolve('uploads'));
